bd4ce7fa9beee8dcd0cd11c3a0805eeafae133a5
Andy Smith <asmit844@ford.com>
2024-03-27 21:25:10 +0000

    add test env var

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 
 export default async function Page() {
 
-
- 
+  const testEnvVar = process.env.TEST_ENV_VAR;
   
   return <div>
-    <h1>Home page</h1>
+    <h1>Home page {testEnvVar}</h1>
   </div>
 }
