1ec713198339d5fcb30466a0d950969081a82059
Andy Smith <asmit844@ford.com>
2024-03-27 21:07:59 +0000

    bare bones nav and styles

---
+++ src/app/ui/navigation.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+import './navigation.css';
+
+export default async function Navigation() {
+
+    return (
+
+        <header className="NavBar">
+            <Link href="/" className="logo-link">
+                <div className="logo logo-font">STREAMS</div>
+            </Link>
+            <div className="menu">
+                <ul>
+                    <li><Link href="/login">Login</Link></li>
+                </ul>
+            </div>
+
+        </header>
+    )
+}
+
+
