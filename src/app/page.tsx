fb34602142a3846e6caf6d16e3d8823d31c1eadd
Andy Smith <asmit844@ford.com>
2024-03-28 15:54:57 +0000

    add Reels

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,13 @@ import { Reel } from "../ui/reel";
 
 export default async function Page() {
 
-
-  const testEnvVar = process.env.TEST_ENV_VAR;
-
   const getPopular: Promise<ITitle[]> = TMDB.getPopular();
   const [popular] = await Promise.all([getPopular]);
 
-
   return <div>
-    <h1>Home page TMDB_TOKEN:{process.env.TMDB_TOKEN}</h1>
-    <h2>TEST_ENV_VAR:{process.env.TEST_ENV_VAR}</h2>
+    <h1>Home page </h1>
     <Reel title="Popular" titles={popular} />
+    <Reel title="Trending" titles={trending} />
+    <Reel title="Christmas" titles={christmas} />
   </div>
 }
