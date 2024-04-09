ddcd3a1802add38086299dbc588fd5ab7453a836
Andy Smith <asmit844@ford.com>
2024-04-09 15:21:48 +0100

    add autocomplete with debounce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { isAuthenticatd } from "@/apis/supabase";
 import { TMDB } from "../apis/TMDB";
 import { ITitle } from "../data/ITitle";
 import { Reel } from "../ui/reel";
+import SearchUI from "@/ui/search-ui";
+
 export const dynamic = 'force-dynamic'
 
 export default async function Page() {
@@ -9,8 +12,9 @@ export default async function Page() {
   const getTrending: Promise<ITitle[]> = TMDB.getTrending();
   const getChristmas: Promise<ITitle[]> = TMDB.searchTitles('christmas');
   const [popular, trending, christmas] = await Promise.all([getPopular, getTrending, getChristmas]);
-  
+
   return <div>
+    <SearchUI />
     <Reel title="Popular" titles={popular} />
     <Reel title="Trending" titles={trending} />
     <Reel title="Christmas" titles={christmas} />
---
+++ src/ui/autocomplete.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React from 'react'
+import AutoCompleteItem from './autocomplete-item'
+import './autocomplete.css'
+import { ITitle } from '../data/ITitle'
+
+
+type AutoCompleteProps = {
+    results: ITitle[]
+}
+
+export default function AutoComplete(props: AutoCompleteProps) {
+
+    if (!props.results.map) {
+
+    }
+    const resultsList = props.results.map(result => <li key={result.id}><AutoCompleteItem result={result}></AutoCompleteItem></li>)
+
+    if (props.results.length === 0) {
+        return <></>;
+    }
+
+    return (
+        <div className="autocomplete">
+            <ul>
+                {resultsList}
+            </ul>
+        </div>
+    )
+}
